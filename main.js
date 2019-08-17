506202fdfb753725c7acfef2cda6d74df4b9b68a
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-08-17 12:41:04 -0600

    Add working JS, remove html div and styling

--- main.js
+++ main.js
@@ -4,25 +4,5 @@ var userDogName = document.querySelector('#name_this_dog_button');
 var updateName = document.querySelector('#some_dogs');
 
 changeDogName.addEventListener('click', function() {
-  // console.log(nameInput.value);
-  // console.log(spanTest.innerText);
-  updateName.innerText=nameInput.value;
+  updateName.innerText = nameInput.value;
 });
-
-
-
-
-// changeDogName.addEventListener('click', function() {
-//
-// });
-//
-// console.log(nameInput.innerText);
-
-// document.querySelector('#some_dogs').innerText = ' how much I wish I could use flexbox';
-
-
-//user enters name
-//user clicks name this dog button
-//new name is value of variable
-// variable is in innertext
-// returns innertext with variable value

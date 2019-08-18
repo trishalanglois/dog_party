a3480845107981f9d180be39af26e650855caaf8
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-08-18 08:29:40 -0600

    Add EventListeners to each individual column

--- main.js
+++ main.js
@@ -7,13 +7,40 @@ changeDogName.addEventListener('click', function() {
   updateName.innerText = nameInput.value;
 });
 
-var hideParagraph = document.getElementById('left_bottom_button');
+var hideParagraph1 = document.getElementById('left_bottom_button');
+var hideParagraph2 = document.getElementById('middle_bottom_button');
+var hideParagraph3 = document.getElementById('right_bottom_button');
+// function hideParagraph (paragraphNumber) {
+//   if (paragraphNumber.style.display === 'none') {
+//     paragraphNumber.style.display = 'block';
+//   } else {
+//     paragraphNumber.style.display = 'none'
+//   }
+// });
 
-hideParagraph.addEventListener('click', function() {
-  var paragraphToHide = document.querySelector('.column_paragraph');
-  if (paragraphToHide.style.display === 'none') {
-    paragraphToHide.style.display = "block";
+hideParagraph1.addEventListener('click', function() {
+  var paragraph1ToHide = document.getElementById('column_paragraph1');
+  if (paragraph1ToHide.style.display === 'none') {
+    paragraph1ToHide.style.display = 'block';
   } else {
-    paragraphToHide.style.display = "none";
+    paragraph1ToHide.style.display = 'none';
+  }
+});
+
+hideParagraph2.addEventListener('click', function(){
+  var paragraph2ToHide = document.getElementById('column_paragraph2');
+  if (paragraph2ToHide.style.display === 'none') {
+    paragraph2ToHide.style.display = 'block';
+  } else {
+    paragraph2ToHide.style.display = 'none';
+  }
+});
+
+hideParagraph3.addEventListener('click', function() {
+  var paragraph3ToHide = document.getElementById('column_paragraph3');
+  if (paragraph3ToHide.style.display === 'none') {
+    paragraph3ToHide.style.display = 'block';
+  } else {
+    paragraph3ToHide.style.display = 'none';
   }
 });

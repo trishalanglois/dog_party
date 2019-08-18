99c84a9aa7ecb156784af7c1fece686f77f0ccdf
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-08-18 15:06:52 -0600

    Re-refactor with individual functions for hiding paragraphs

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
-var changeDogName = document.querySelector('#name_this_dog_button');
-var nameInput = document.getElementById('dog_name_box');
-var userDogName = document.querySelector('#name_this_dog_button');
-var updateName = document.querySelector('#some_dogs');
+var changeDogName = document.querySelector('#name-this-dog-button');
+var nameInput = document.getElementById('dog-name-box');
+var updateName = document.querySelector('#some-dogs');
 
 changeDogName.addEventListener('click', function() {
   updateName.innerText = nameInput.value;
 });
 
-var hideParagraph1 = document.getElementById('left_bottom_button');
-var hideParagraph2 = document.getElementById('middle_bottom_button');
-var hideParagraph3 = document.getElementById('right_bottom_button');
-function hideParagraph (paragraphNumber) {
-  if (paragraphNumber.style.display === 'none') {
-    paragraphNumber.style.display = 'block';
+var hideParagraph1 = document.getElementById('left-bottom-button');
+var hideParagraph2 = document.getElementById('middle-bottom-button');
+var hideParagraph3 = document.getElementById('right-bottom-button');
+
+hideParagraph1.addEventListener('click', function(){
+  var columnParagraph1 = document.getElementById('column-paragraph1');
+
+  if(columnParagraph1.style.display === 'none') {
+    columnParagraph1.style.display = 'block';
   } else {
-    paragraphNumber.style.display = 'none'
+    columnParagraph1.style.display = 'none';
   }
 });
 
-hideParagraph ('column_paragraph1');
+hideParagraph2.addEventListener('click', function(){
+  var columnParagraph2 = document.getElementById('column-paragraph2');
 
-hideParagraph('column_paragraph2');
+  if(columnParagraph2.style.display === 'none') {
+    columnParagraph2.style.display = 'block';
+  } else {
+    columnParagraph2.style.display = 'none';
+  }
+});
 
-hideParagraph('column_paragraph3');
+hideParagraph3.addEventListener('click', function(){
+  var columnParagraph3 = document.getElementById('column-paragraph3');
+
+  if(columnParagraph3.style.display === 'none') {
+    columnParagraph3.style.display = 'block';
+  } else {
+    columnParagraph3.style.display = 'none';
+  }
+});
